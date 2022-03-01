6db40ade72f4a20971244b3de34cf591bf43ae51
Mutyala Santosh <santoshmutyala@zemosolabs.com>
2022-03-02 03:02:48 +0530

    Using Qualifier annotation

--- src/com/luv2code/springdemo/RandomFortuneService.java
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -2,12 +2,27 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomFortuneService implements FortuneService{
 
+    // create an array of strings
+    private String[] data = {
+            "Hello", "Guten Tag", "Namaste"
+    };
+
+    // create a random number generator
+    private Random myRandom = new Random();
+
     @Override
     public String getFortune() {
-        return null;
+        //pick a random string from the array
+        int index = myRandom.nextInt(data.length);
+
+        String theFortune = data[index];
+
+        return theFortune;
     }
 
 }
