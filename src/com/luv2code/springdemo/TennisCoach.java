2442629973667627f6c9feff67f66ebd8cf604ff
Mutyala Santosh <santoshmutyala@zemosolabs.com>
2022-03-02 00:43:31 +0530

    Default component name

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component // default Component == tennisCoach
 public class TennisCoach implements Coach{
 
     @Override
